5d152ba29e0d1122281e32dce371bb45c936dd7a
shuvo794 <shuvohossain794@gmail.com>
2022-11-23 21:14:57 +0600

    send

--- array_project.c
+++ array_project.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-
+    printf("Menu : ");
+    printf("--------------------\n");
+    printf("1.Add\n");
+    printf("2.Insert\n");
+    printf("3.Edist\n");
+    printf("4.Delete\n");
+    printf("5.Display\n");
+    printf("6.clear\n");
+    printf("0.Exist\n");
     return 0;
 }
