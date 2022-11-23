1d8d1c2d3d9a6ff925b3ea6417808b1d9a3caabf
shuvo794 <shuvohossain794@gmail.com>
2022-11-23 21:15:13 +0600

    send

--- array_project.c
+++ array_project.c
@@ -5,7 +5,7 @@ int main(){
     printf("--------------------\n");
     printf("1.Add\n");
     printf("2.Insert\n");
-    printf("3.Edist\n");
+    printf("3.Edit\n");
     printf("4.Delete\n");
     printf("5.Display\n");
     printf("6.clear\n");
